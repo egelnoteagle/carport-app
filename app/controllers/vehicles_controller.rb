72a78f09d6ffda19577b2c8c4aeec0d9d362321c
degel55 <chicubfan@gmail.com>
2016-10-03 22:00:40 -0500

    added navbar. created vehicle create and new. still new views for them

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -6,4 +6,26 @@ class VehiclesController < ApplicationController
   def show
     @vehicle = Vehicle.find(params[:id])
   end
+
+  def new
+    @vehicle = Vehicle.new
+  end
+
+  def create
+    @vehicle = Vehicle.create(
+                        make: params[:make],
+                        model: params[:model], 
+                        year: params[:year],
+                        trim: params[:trim],
+                        style: params[:style],
+                        color: params[:color],
+                        vin_num: params[:vin_num],
+                        vehicle_nickname: params[:vehicle_nickname],
+                        license_state: params[:license_state],
+                        license_number: params[:license_number]
+                        )
+
+    flash[:success] = "New Vehicle Saved!"
+    redirect_to "/vehicles#{@vehicle.id}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get '/mygarage' => 'vehicles#index'
   get '/vehicles/:id' => 'vehicles#show'
+
+  get '/vehicles/new' => 'vehicles#new'
+  post '/vehicles/' => 'vehicles#create'
 end
