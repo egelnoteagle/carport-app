7b20a5dd97dedda722d30d4464d8cd1bfca2cb5f
degel55 <chicubfan@gmail.com>
2016-10-04 23:34:41 -0500

    finished vehicles REST MVC basic. starting users

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -12,7 +12,7 @@ class VehiclesController < ApplicationController
   end
 
   def create
-    @vehicle = Vehicle.create(
+    @vehicle = Vehicle.new(
                         make: params[:make],
                         model: params[:model], 
                         year: params[:year],
@@ -24,8 +24,39 @@ class VehiclesController < ApplicationController
                         license_state: params[:license_state],
                         license_number: params[:license_number]
                         )
+    @vehicle.save
 
     flash[:success] = "New Vehicle Saved!"
-    redirect_to "/vehicles#{@vehicle.id}"
+    redirect_to "/vehicles/#{@vehicle.id}"
+  end
+
+  def edit
+    @vehicle = Vehicle.find(params[:id])
+  end
+
+  def update
+    @vehicle = Vehicle.find(params[:id])
+    @vehicle.update(
+                                make: params[:make],
+                                model: params[:model], 
+                                year: params[:year],
+                                trim: params[:trim],
+                                style: params[:style],
+                                color: params[:color],
+                                vin_num: params[:vin_num],
+                                vehicle_nickname: params[:vehicle_nickname],
+                                license_state: params[:license_state],
+                                license_number: params[:license_number])
+
+    flash[:success] = "Vehicle Updated"
+    redirect_to "/vehicles/#{@vehicle.id}"
+  end
+
+  def destroy
+    @vehicle = Vehicle.find(params[:id])
+    @vehicle.destroy
+
+    flash[:warning] = "Vehicle Removed"
+    redirect_to "/mygarage"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   get '/mygarage' => 'vehicles#index'
-  get '/vehicles/:id' => 'vehicles#show'
 
   get '/vehicles/new' => 'vehicles#new'
-  post '/vehicles/' => 'vehicles#create'
+  post '/vehicles' => 'vehicles#create'
+
+  get '/vehicles/:id' => 'vehicles#show'
+
+  get '/vehicles/:id/edit' => 'vehicles#edit'
+  patch '/vehicles/:id' => 'vehicles#update'
+
+  delete '/vehicles/:id' => 'vehicles#destroy'
 end
