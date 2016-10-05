9ae154d96ea90a8cca163e927b32a7592f077d11
degel55 <chicubfan@gmail.com>
2016-10-05 00:20:21 -0500

    created auth sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,5 +16,13 @@ class UsersController < ApplicationController
                 city: params[:city],
                 state: params[:state],
                 zip: params[:zip])
+    if user.save
+      session[:user_id] = user.id
+      flash[:success] = 'Account Created'
+      redirect_to '/mygarage'
+    else
+      flash[:warning] = 'Invalid email or password'
+      redirect_to '/signup'
+    end    
   end
 end
